e68c63e8d412c0733c188fdb9ca762bbe8d215fc
125AritraBasakJoy <basakjoy125@gmail.com>
2025-09-11 18:11:46 +0600

    First commit - AngularTutorial project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { User } from './components/user/user';
+import { Admin } from './components/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'home',
+    component: Admin
+},
+{
+    path: 'homie',
+    component: User  
+}];
